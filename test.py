3ffdde0b21ba4ae183607a5473d6d8e1db9a5f4e
blakeyi <blakeyi@tencent.com>
2023-11-01 23:45:29 +0800

    333

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ def on_msg(msg):
 bot = Bot(ip='127.0.0.1', port=5555)
 bot.register("on_open", lambda ws: logging.info("hi"))
 bot.register("on_close", lambda ws: logging.info("bye"))
-bot.register("recv_txt_msg", lambda msg: on_msg(msg))
+bot.register("recv_txt_msg", lambda msg: logging.info("msg"))
 # run event loop
 bot.run()
