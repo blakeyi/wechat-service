5ff7a07d11f08f3fc8d82c3b225a1f1a019ebdd5
blakeyi <blakeyi@tencent.com>
2023-11-01 23:46:51 +0800

    222

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-from wesdk import Bot
-import logging
-
-def on_msg(msg):
-    logging.info("hi")
-
+from wesdk import *
 # connect to hook service
 bot = Bot(ip='127.0.0.1', port=5555)
-bot.register("on_open", lambda ws: logging.info("hi"))
-bot.register("on_close", lambda ws: logging.info("bye"))
-bot.register("recv_txt_msg", lambda msg: logging.info("msg"))
+bot.register("on_open", lambda ws: logging("hi"))
+bot.register("on_close", lambda ws: logging("bye"))
 # run event loop
 bot.run()
