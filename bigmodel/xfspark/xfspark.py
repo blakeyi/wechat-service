9d9b2fbe6ddc21a7a5a547451f191698c8da3784
blakeyi <blakeyi@tencent.com>
2023-11-02 00:46:53 +0800

    讯飞大模型修改为3.1版本

--- bigmodel/xfspark/xfspark.py
+++ bigmodel/xfspark/xfspark.py
@@ -7,10 +7,14 @@ api_key ="a2609ec9ba4ff8653399460b1a0724f8"    #填写控制台中获取的 APIK
 
 #用于配置大模型版本，默认“general/generalv2”
 # domain = "general"   # v1.5版本
-domain = "generalv2"    # v2.0版本
+# domain = "generalv2"    # v2.0版本
+domain = "generalv3"    # v3.0版本
+
 #云端环境的服务地址
 # Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
-Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
+# Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
+Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat"  # v2.0环境的地址
+
 
 
 text =[]
