24dc7a3f83a3147d8ac5b241e27d176fabbc5619
blakeyi <blakeyi@tencent.com>
2023-11-01 23:34:06 +0800

    222

--- wesdk/bot.py
+++ wesdk/bot.py
@@ -266,7 +266,7 @@ class Bot(threading.Thread):
         return on_close
     def make_on_message(self):
         def on_message(ws, message):
-            print("on_message", message)
+            print("on_message1", message)
             if 'content' in j and isinstance(j['content'], str):
                 try:
                     j['content'] = json.loads(j['content'])
