33d8e7acf7d0722d1ddf215318b59c47be21e610
blakeyi <blakeyi@tencent.com>
2023-11-01 23:51:27 +0800

    22

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from wesdk import *
 # connect to hook service
 def on_msg(msg):
-    logging(msg)
+    logging("msg ", msg)
 
 bot = Bot(ip='127.0.0.1', port=5555)
 bot.register("on_open", lambda ws: logging("hi"))
