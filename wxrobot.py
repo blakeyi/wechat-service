6723d677d6c3f1056d618469b5e237692a88e180
blakeyi <blakeyi@tencent.com>
2023-11-02 09:18:35 +0800

    修复回复问题

--- wxrobot.py
+++ wxrobot.py
@@ -5,12 +5,14 @@ from bigmodel.baidu.ERNIE import getAnswer as getBaiduAnswer
 # connect to hook service
 def on_msg(msg):
     logging(msg)
-    ans = getXunFeiAnswer(str(msg["content"]).replace("@masteryi", ""))
-    if msg["roomid"] and "@masteryi" in str(msg["content"]):
+    content = str(msg["content"])
+    if msg["roomid"] and "@masteryi" in content:
+        ans = getXunFeiAnswer(content.replace("@masteryi", ""))
         nickname = bot.get_chatroom_member_nick(msg["roomid"], msg['senderid'])
         print("nickname", nickname)
         bot.send_msg(ans, msg['senderid'], msg["roomid"], nickname)
     else:
+        ans = getXunFeiAnswer(content)
         bot.send_msg(ans, msg['wxid'])
 
 bot = Bot(ip='127.0.0.1', port=5555)
